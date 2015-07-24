c836dca3b0c9b9dff2c82f22d6e27edcf5c8cb17
souki_tn <souki@panini.sakura.ne.jp>
2015-07-24 16:43:08 +0900

    強制フランス語化達成

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,12 @@
 package controllers
 
+import javax.inject.Inject
 import play.api._
 import play.api.mvc._
+import play.api.i18n._
 
-class Application extends Controller {
-
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
+class Application @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
+	def index = Action {
+		Ok(views.html.index("Your new application is ready.")(messagesApi.preferred(Seq(new Lang("fr")))))
+	}
 }
